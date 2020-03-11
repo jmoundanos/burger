cfdf62597d70a852398985dd74250ea7aeeb5462
Jennifer Moundanos <jmoundanos@gmail.com>
2020-03-11 10:58:19 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES("The Horseshoe", true);
+INSERT INTO burgers (burger_name, devoured) VALUES("The Superbird", true);
+INSERT INTO burgers (burger_name, devoured) VALUES("The Rosedale", true);
